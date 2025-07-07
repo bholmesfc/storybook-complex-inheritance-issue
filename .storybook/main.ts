1c194c5bec3f1e2e8945f98dcb0c4c5729f3d1da
bholmesfc <bholmes@fingercheck.com>
2025-07-07 14:44:20 -0400

    Create main.ts

---
+++ .storybook/main.ts
@@ -0,0 +1,17 @@
+import type { StorybookConfig } from '@storybook/angular';
+
+const config: StorybookConfig = {
+  stories: ['../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  addons: [
+    '@storybook/addon-essentials',
+  ],
+  framework: {
+    name: '@storybook/angular',
+    options: {},
+  },
+  docs: {
+    autodocs: 'tag',
+  },
+};
+
+export default config;
